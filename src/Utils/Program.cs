9a41c4a11f464efaab74d257a4eb2360f9f6b4e9
Ernst Fluttert <e.fluttert@gmail.com>
2016-01-03 12:35:53 +0100

    Cleared main

--- src/Utils/Program.cs
+++ src/Utils/Program.cs
@@ -12,20 +12,7 @@ namespace ChallengeUtilsUtils
 		static void Main(string[] args)
 		{
 
-			Node<string> t1 = new Node<string>("test2", "Node2");
-			Node<string> t2 = new Node<string>("test3", "Node3");
-			Node<string> newChild2 = new Node<string>("test5", "Node5");
-			Node<string> newChild1 = new Node<string>("test4", "Node4");
-			Node<string> root = new Node<string>("test1", null, "RootNode");
-
-			newChild1.AddExistingChild(newChild2);
-			root.AddExistingChild(t1).AddExistingChild(t2).AddExistingChild(newChild1);
-			var res = root.Select(a => a);
-			foreach (var abc in res) {
-				Console.WriteLine(abc.NodeId);
-			}
-
-			Console.ReadLine();
+			// INTENTIONALLY LEFT BLANK
 		}
 	}
 }
