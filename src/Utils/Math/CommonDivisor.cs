626af38965284efa5468cc43edb04b9437e5d5fc
Ernst Fluttert <e.fluttert@gmail.com>
2016-01-03 22:24:12 +0100

    Added sorting + UnitTests for common-divisors

--- src/Utils/Math/CommonDivisor.cs
+++ src/Utils/Math/CommonDivisor.cs
@@ -2,61 +2,63 @@
 
 namespace ChallengeUtils.Math
 {
-	public class CommonDivisor
-	{
-		/// <summary>Get all common divisors between 2 numbers (long/int64)</summary>
-		/// <param name="a">Number</param>
-		/// <param name="b">Number</param>
-		/// <returns>List of common divisors (empty on 0 or negative numbers)</returns>
-		public static List<long> CommonDivisors(long a, long b)
-		{
-			// enforce a <= b
-			if (a > b) { long tmp = a; a = b; b = tmp; }
-			List<long> commonDivisors = new List<long>();
-			// do a zero check and negativity check
-			if (a <= 0 || b <= 0) { return commonDivisors; }
-			// a>0 and b>0 so it will be divisble by 1 :D
-			commonDivisors.Add(1);
-			// if a == 1, return
-			if (a == 1) { return commonDivisors; }
-			// if a is a divisor, add it. eg a=3, b=6
-			if (b % a == 0) { commonDivisors.Add(a); }
-			// start at halve the range
-			for (long i = (a / 2) + 1; i > 1; i--)
-			{   // it is a common divisor if both a and b are divisible by it (duh)
-				if (a % i == 0 && b % i == 0)
-				{   // this is our lucky day!
-					commonDivisors.Add(i);
-				}
-			}
-			return commonDivisors;
-		}
+    public class CommonDivisor
+    {
+        /// <summary>Get all common divisors between 2 numbers (long/int64)</summary>
+        /// <param name="a">Number</param>
+        /// <param name="b">Number</param>
+        /// <returns>List of common divisors (empty on 0 or negative numbers)</returns>
+        public static List<long> CommonDivisors(long a, long b)
+        {
+            // enforce a <= b
+            if (a > b) { long tmp = a; a = b; b = tmp; }
+            List<long> commonDivisors = new List<long>();
+            // do a zero check and negativity check
+            if (a <= 0 || b <= 0) { return commonDivisors; }
+            // a>0 and b>0 so it will be divisble by 1 :D
+            commonDivisors.Add(1);
+            // if a == 1, return
+            if (a == 1) { return commonDivisors; }
+            // if a is a divisor, add it. eg a=3, b=6
+            if (b % a == 0) { commonDivisors.Add(a); }
+            // start at halve the range
+            for (long i = (a / 2) + 1; i > 1; i--)
+            {   // it is a common divisor if both a and b are divisible by it (duh)
+                if (a % i == 0 && b % i == 0)
+                {   // this is our lucky day!
+                    commonDivisors.Add(i);
+                }
+            }
+            // sort the common divisors in ascending order
+            commonDivisors.Sort();
+            return commonDivisors;
+        }
 
-		/// <summary>Classic GCD</summary>
-		/// <param name="a">First number</param>
-		/// <param name="b">Second number</param>
-		/// <returns>Long: biggest common divisor, or 0 (zero) on negative numbers and 0</returns>
-		public static long GreatestCommonDivisor(long a, long b)
-		{
-			// do a zero check and negativity check
-			if (a <= 0 || b <= 0) { return 0; }
-			// biggest divisor possible, when numbers are the same
-			if (a == b) { return a; }
-			// when a or b =1, then it will always be the biggest
-			if (a == 1 || b == 1) { return 1; }
+        /// <summary>Classic GCD</summary>
+        /// <param name="a">First number</param>
+        /// <param name="b">Second number</param>
+        /// <returns>Long: biggest common divisor, or 0 (zero) on negative numbers and 0</returns>
+        public static long GreatestCommonDivisor(long a, long b)
+        {
+            // do a zero check and negativity check
+            if (a <= 0 || b <= 0) { return 0; }
+            // biggest divisor possible, when numbers are the same
+            if (a == b) { return a; }
+            // when a or b =1, then it will always be the biggest
+            if (a == 1 || b == 1) { return 1; }
 
-			// else do the classic Euclidean GCD algorithm make sure a = biggest, b = smallest
-			if (b > a) { long t = a; a = b; b = t; }
+            // else do the classic Euclidean GCD algorithm make sure a = biggest, b = smallest
+            if (b > a) { long t = a; a = b; b = t; }
 
-			// https://en.wikipedia.org/wiki/Euclidean_algorithm. Divide the remainders, until the
-			// first factor is found. This first factor will be the biggest factor
-			while (b != 0)
-			{
-				long temp = b;
-				b = a % b;
-				a = temp;
-			}
-			return a;
-		}
-	}
+            // https://en.wikipedia.org/wiki/Euclidean_algorithm. Divide the remainders, until the
+            // first factor is found. This first factor will be the biggest factor
+            while (b != 0)
+            {
+                long temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+    }
 }
---
+++ src/UtilsTests/Math/CommonDivisorTest.cs
@@ -0,0 +1,43 @@
+﻿using ChallengeUtils.Math;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace UtilsTests.Math
+{
+    [TestClass]
+    public class CommonDivisorTest
+    {
+        [TestMethod]
+        public void CommonDivisorsTest()
+        {
+            // empty list on zero
+            CollectionAssert.AreEqual(new List<long>(), CommonDivisor.CommonDivisors(0, 1));
+
+            // empty list on negative number
+            CollectionAssert.AreEqual(new List<long>(), CommonDivisor.CommonDivisors(-1, 1));
+
+            // a or b = 1
+            CollectionAssert.AreEqual(new List<long>() { 1 }, CommonDivisor.CommonDivisors(18, 1));
+
+            // When a is a factor of b
+            CollectionAssert.AreEqual(new List<long>() { 1, 3 }, CommonDivisor.CommonDivisors(3, 6));
+
+            // normal mode with multiple elements (tests the sorting)
+            CollectionAssert.AreEqual(new List<long>() { 1, 2, 3, 6 }, CommonDivisor.CommonDivisors(6, 18));
+        }
+
+        [TestMethod]
+        public void GreatestCommonDivisorTest()
+        {
+            Assert.AreEqual(0, CommonDivisor.GreatestCommonDivisor(-1, 1));  // edgecase negative numbers
+            Assert.AreEqual(0, CommonDivisor.GreatestCommonDivisor(0, 1));  // edgecase with zero
+            Assert.AreEqual(1, CommonDivisor.GreatestCommonDivisor(1, 1));  // edgecase 1
+            Assert.AreEqual(2, CommonDivisor.GreatestCommonDivisor(2, 4));  // regular numbers
+            Assert.AreEqual(2, CommonDivisor.GreatestCommonDivisor(4, 2));  // reverse order of input
+            Assert.AreEqual(1, CommonDivisor.GreatestCommonDivisor(5, 11)); // primes, no common divisor
+            Assert.AreEqual(1, CommonDivisor.GreatestCommonDivisor(6, 35)); // co-primes (2*3 and 5*7), no common divisor
+            Assert.AreEqual(15, CommonDivisor.GreatestCommonDivisor(30, 45));
+            Assert.AreEqual(21, CommonDivisor.GreatestCommonDivisor(1071, 462)); // wikipedia example
+        }
+    }
+}
